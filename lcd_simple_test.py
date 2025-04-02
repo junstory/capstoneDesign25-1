476ebd70d3a713abffb9038539c864d30fda2a20
junstory <lastcom1@gmail.com>
2025-04-02 22:38:23 +0900

    Another lcd_test

--- lcd_simple_test.py
+++ lcd_simple_test.py
@@ -1,5 +1,15 @@
-import RPi_I2C_driver
 import time
+from LCD import LCD
 
-mylcd = RPi_I2C_driver.lcd()
-mylcd.lcd_display_string("Hello!", 1)
+# Initialize the LCD with specific parameters: Raspberry Pi revision, I2C address, and backlight status
+lcd = LCD(2, 0x27, True)  # Using Raspberry Pi revision 2 and above, I2C address 0x3f, backlight enabled
+
+# Display messages on the LCD
+lcd.message("NerdCave", 1)        # Display 'NerdCave!' on line 1
+lcd.message("Tutorials", 2)    # Display '    - Tutoarials' on line 2
+
+# Keep the messages displayed for 5 seconds
+time.sleep(5)
+
+# Clear the LCD display
+lcd.clear()
