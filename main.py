a4f96cb63f89dcd44ed4a92677695963597756c7
junstory <lastcom1@gmail.com>
2025-04-02 22:13:18 +0900

    Error fix

--- main.py
+++ main.py
@@ -1,20 +1,50 @@
+import os
 import RPi_I2C_driver
 import time
  
 mylcd = RPi_I2C_driver.lcd()
- 
-i = 0
-while True:
 
+
+# ds18b20 온도데이터값을 아래 temp_sensor 경로에 저장하기 위한 명령어
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+# 라즈베리파이가 센서데이터를 받는 경로를 설정
+temp_sensor='/sys/bus/w1/devices/28-0000003860f9/w1_slave'
+ 
+# 파일의 내용을 읽어오는 함수
+def temp_raw():
+    f = open(temp_sensor,'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+# 읽어온 파일의 구문을 분석해 온도부분만 반환하는 함수
+def read_temp():
+    lines = temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = temp_raw()
+ 
+ 
+    temp_output = lines[1].find('t=')
+ 
+    if temp_output != -1:
+        temp_string = lines[1].strip()[temp_output+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0/5.0 + 32.0
+        return temp_c, temp_f
+ 
+ 
+while True:
+    print(read_temp())
+    
     now = time.localtime()
     current_time = time.strftime("%H:%M:%S", now)
-    print(current_time)
 
     mylcd.lcd_display_string("Current Time: " + current_time, 1)
-    mylcd.lcd_display_string("Change Check: ", i)
-    mylcd.lcd_display_string("Hello World!")
-    time.sleep(2)
-    mylcd.lcd_clear()
+    mylcd.lcd_display_string("Temp: %.3f C" % read_temp()[0], 2)
+    mylcd.lcd_display_string("Temp: %.3f F" % read_temp()[1], 3)
     time.sleep(1)
-    i += 1
+    mylcd.lcd_clear()
 
