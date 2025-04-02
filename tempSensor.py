49c3dfaa9dad113630ee541d136b0d0663715e88
junstory <lastcom1@gmail.com>
2025-04-02 22:04:14 +0900

    error 수정

--- tempSensor.py
+++ tempSensor.py
@@ -2,7 +2,7 @@ import os
 import RPi_I2C_driver
 import time
  
-cld = RPi_I2C_driver.lcd()
+mylcd = RPi_I2C_driver.lcd()
 
 
 # ds18b20 온도데이터값을 아래 temp_sensor 경로에 저장하기 위한 명령어
@@ -41,7 +41,7 @@ while True:
     
     now = time.localtime()
     current_time = time.strftime("%H:%M:%S", now)
-    
+
     mylcd.lcd_display_string("Current Time: " + current_time, 1)
     mylcd.lcd_display_string("Temp: %.3f C" % read_temp()[0], 2)
     mylcd.lcd_display_string("Temp: %.3f F" % read_temp()[1], 3)
