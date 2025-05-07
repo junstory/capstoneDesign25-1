c2ecf85be1eea24e9c6242fbd2df589fadce815b
junstory <lastcom1@gmail.com>
2025-05-07 23:57:30 +0900

    ads1115 loop code

--- ads1115_test.py
+++ ads1115_test.py
@@ -6,5 +6,7 @@ from adafruit_ads1x15.analog_in import AnalogIn
 i2c = busio.I2C(board.SCL, board.SDA)
 
 ads = ADS.ADS1115(i2c)
-chan = AnalogIn(ads, ADS.P0)
-print(chan.value, chan.voltage)
+if _name__ == "__main__":
+    while True:
+        chan = AnalogIn(ads, ADS.P0)
+        print(chan.value, chan.voltage)
