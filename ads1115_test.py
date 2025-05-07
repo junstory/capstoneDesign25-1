1901636f5e7cd1b662d9aff09502b2cf66f6bf36
junstory <lastcom1@gmail.com>
2025-05-07 23:57:56 +0900

    ads1115 time sleep added

--- ads1115_test.py
+++ ads1115_test.py
@@ -2,6 +2,7 @@ import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+import time
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
@@ -10,3 +11,4 @@ if _name__ == "__main__":
     while True:
         chan = AnalogIn(ads, ADS.P0)
         print(chan.value, chan.voltage)
+        time.sleep(1)
